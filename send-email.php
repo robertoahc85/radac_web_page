595dd360b200e3ef1597b4f2882d9a7791eb14eb
robertoahc85 <robertoahc85@gmail.com>
2022-10-28 18:40:34 -0500

    comment testimonial

--- send-email.php
+++ send-email.php
@@ -4,10 +4,10 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "robertocapi36@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$subject = "Email del sitio web";
+$txt ="Nombre = ". $name . "\r\n  Email = " . $email . "\r\n Mensaje =" . $message;
+$headers = "From: noreply@radac.com.mx" . "\r\n" .
+"CC: contact@radac.com.mx";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
