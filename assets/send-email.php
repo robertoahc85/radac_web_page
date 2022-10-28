fde21ebd4c2a60278c440c7d181ceb47cce1ddbf
robertoahc85 <robertoahc85@gmail.com>
2022-10-28 18:09:58 -0500

    Add change of email and analatycs

---
+++ assets/send-email.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.titan.email";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "contacto@radac.com.mx";
+$mail->Password = "$Radac2022$";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("roberto@example.com", "Dave");
+
+$mail->Subject = "Contacto pagina web";
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
+
+?>
