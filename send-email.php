90d711957ede0b1a954ec0bf592f223da6ce29e0
robertoahc85 <robertoahc85@gmail.com>
2022-10-31 16:49:21 -0600

    Add sender email

--- send-email.php
+++ send-email.php
@@ -3,11 +3,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "robertocapi36@gmail.com";
+$to = "josello93@gmail.com";
 $subject = "Email del sitio web";
 $txt ="Nombre = ". $name . "\r\n  Email = " . $email . "\r\n Mensaje =" . $message;
 $headers = "From: noreply@radac.com.mx" . "\r\n" .
-"CC: contact@radac.com.mx";
+"CC: ventas@radac.com.mx";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
