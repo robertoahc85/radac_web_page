cbc38c69cf5b164bade59fd86af385ff2ce371c6
robertoahc85 <robertoahc85@gmail.com>
2022-10-31 18:17:54 -0600

    change sender

--- send-email.php
+++ send-email.php
@@ -7,7 +7,7 @@ $to = "ventas@radac.com.mx";
 $subject = "Email del sitio web";
 $txt ="Nombre = ". $name . "\r\n  Email = " . $email . "\r\n Mensaje =" . $message;
 $headers = "From: noreply@radac.com.mx" . "\r\n" .
-"CC:josello93@gmail.com;";
+"CC:robertocapi36@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
